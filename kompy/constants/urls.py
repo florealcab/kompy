6de4a66daeeab649a501c808f3fe4c81c9c77ba4
Floréal Cabanettes <comm@alypie.fr>
2024-08-14 21:10:48 +0200

    Add function to update title of a tour

--- kompy/constants/urls.py
+++ kompy/constants/urls.py
@@ -5,4 +5,5 @@ class KomootUrl:
     USER_LOGIN_URL: Final[str] = 'https://api.komoot.de/v006/account/email/{email_address}/'
     LIST_TOURS_URL: Final[str] = 'https://api.komoot.de/v007/users/{user_identifier}/tours/'
     DOWNLOAD_TOUR_URL: Final[str] = 'https://api.komoot.de/v007/tours/{tour_identifier}'
+    UPDATE_TOUR_URL: Final[str] = 'https://api.komoot.de/v007/tours/{tour_identifier}'
     UPLOAD_TOUR_URL: Final[str] = 'https://api.komoot.de/v007/tours/?data_type={object_type}'
